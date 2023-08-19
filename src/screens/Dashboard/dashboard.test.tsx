c1cda09b107238a14c5cd6e9ac9b69b9ce9bda52
Mateus8741 <mateus_sousa_tavares@hotmail.com>
2023-08-19 13:20:53 -0300

    :sparkles: feat: dashboard test

--- src/screens/Dashboard/dashboard.test.tsx
+++ src/screens/Dashboard/dashboard.test.tsx
@@ -1,26 +1,43 @@
+import { mockCityAPIResponse } from "@__tests__/mocks/api/mockCityAPIResponse";
+import { mockWeatherAPIResponse } from "@__tests__/mocks/api/mockWatherAPIResponse";
+import { render, screen, waitFor } from "@__tests__/utils/customRender";
+import { saveStorageCity } from "@libs/asyncStorage/cityStorage";
+import { Dashboard } from "@screens/Dashboard";
+import { api } from "@services/api";
 
- import { mockWeatherAPIResponse } from "@__tests__/mocks/api/mockWatherAPIResponse"
-import { render, screen, waitFor } from "@__tests__/utils/customRender"
-import { saveStorageCity } from "@libs/asyncStorage/cityStorage"
-import { Dashboard } from "@screens/Dashboard"
-import { api } from "@services/api"
+describe("Screen: Dashboard", () => {
+  it("should be show city weather", async () => {
+    jest.spyOn(api, "get").mockResolvedValue({ data: mockWeatherAPIResponse });
 
- describe("Screen: Dashboard", () => {
-   it('should be show city weather', async () => {
-     jest.spyOn(api, 'get').mockResolvedValue({ data: mockWeatherAPIResponse });
+    const city = {
+      id: "1",
+      name: "Rio do Sul, BR",
+      latitude: 123,
+      longitude: 456,
+    };
 
-     const city = {
-       id: '1',
-       name: 'Rio do Sul, BR',
-       latitude: 123,
-       longitude: 456
-     }
+    await saveStorageCity(city);
 
-     await saveStorageCity(city)
+    render(<Dashboard />);
 
-     render(<Dashboard />)
+    const cityName = await waitFor(() => screen.findByText(/rio do sul/i));
+    expect(cityName).toBeTruthy();
+  });
 
-     const cityName = await waitFor(() => screen.findByText(/rio do sul/i));
-     expect(cityName).toBeTruthy() 
-   })
- }) 
+  it("should be show another selected weather city", async () => {
+    const city = {
+      id: "1",
+      name: "Rio do Sul, BR",
+      latitude: 123,
+      longitude: 456,
+    };
+
+    await saveStorageCity(city);
+
+    jest
+      .spyOn(api, "get")
+      .mockResolvedValueOnce({ data: mockWeatherAPIResponse })
+      .mockResolvedValueOnce({ data: mockCityAPIResponse })
+      .mockResolvedValueOnce({ data: mockWeatherAPIResponse });
+  });
+});
