3bac253b3926ba0f500152b499dd5eddd6cedb64
Mateus8741 <mateus_sousa_tavares@hotmail.com>
2023-08-21 12:09:42 -0300

    refactor: Test search

--- src/screens/Search/search.test.tsx
+++ src/screens/Search/search.test.tsx
@@ -1,25 +1,17 @@
-import { mockCityAPIResponse } from "@__tests__/mocks/api/mockCityAPIResponse";
-import {
-  fireEvent,
-  render,
-  screen,
-  waitFor,
-} from "@__tests__/utils/customRender";
-import { Search } from "@screens/Search";
-import { api } from "@services/api";
+import { mockCityAPIResponse } from "@__tests__/mocks/api/mockCityAPIResponse"
+import { fireEvent, render, screen, waitFor } from "@__tests__/utils/customRender"
+import { Search } from "@screens/Search"
+import { api } from "@services/api"
 
-describe("Screen: Search", () => {
-  it("should be show city option", async () => {
-    jest.spyOn(api, "get").mockResolvedValue({ data: mockCityAPIResponse });
-
-    render(<Search />);
-
-    const searchInput = screen.getByTestId("search-input");
-    fireEvent.changeText(searchInput, "São Paulo");
-
-    await waitFor(() => expect(screen.findByText(/são paulo/i, {}, { timeout: 3000 })).toBeTruthy());
+describe('Screen: Search', () => {
+  it('should be show city option', async () => {
+    jest.spyOn(api, "get").mockResolvedValue({ data: mockCityAPIResponse })
     
+    render(<Search />)
 
-    // await waitFor(() => expect(screen.findByText(/rio do sul/i, {}, { timeout: 3000 })).toBeTruthy);
-  });
-});
+    const searchInput = screen.getByTestId('search-input')
+    fireEvent.changeText(searchInput, "São Paulo")
+
+    await waitFor(() => expect(screen.findByText(/são paulo/i, {}, { timeout: 3000 })).toBeTruthy())
+  })
+})
